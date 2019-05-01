1239fb9c15810b57a4fdf399e0aa6f2c6931e628
Tuan <tuanpm@live.com>
2019-05-01 19:43:06 +0700

    Initial project

---
+++ README.md
@@ -0,0 +1,2 @@
+# BLUFI Android module
+
---
+++ src/main/java/com/espressif/blufi/BlufiUtils.java
@@ -0,0 +1,12 @@
+package com.espressif.blufi;
+
+public class BlufiUtils {
+    public static void sleep(long timeout) {
+        try {
+            Thread.sleep(timeout);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+}
